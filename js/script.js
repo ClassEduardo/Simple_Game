008b85e238910b74d8179445ca4005ef784af363
Eduardo Gonç <dudue0578@gmail.com>
2022-06-25 17:54:04 -1200

    Botões start e reset

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ class GameJumpMario {
       this.game_board = document.querySelector('.game_board')
       this.callJump();
       this.loopGame();
+      this.addClass();
+   }
+   
+   addClass() {
+      const pipe = this.game_board.querySelector('.pipe');
+
+      pipe.style.animation = 'pipe_animation 1.4s linear infinite';
+      pipe.style.display = 'inherit';
    }
 
    callJump() {
@@ -13,9 +21,8 @@ class GameJumpMario {
    
    jump() {      
       const mario = this.game_board.querySelector('.mario');
-
       mario.classList.add('jump')
-         setTimeout(() => {
+         setTimeout(() => { 
             mario.classList.remove('jump')
          }, 530)
    }
@@ -23,12 +30,13 @@ class GameJumpMario {
    loopGame(){
       const pipe = this.game_board.querySelector('.pipe');
       const mario = this.game_board.querySelector('.mario');
+      const buttonReset = document.querySelector('.restart');
 
       const loop = setInterval(() => { 
          const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
          const pipePosition = pipe.offsetLeft;
-         
-         if(pipePosition <= 132&& pipePosition > 0 && marioPosition < 105) {
+
+         if(pipePosition <= 132 && pipePosition > 0 && marioPosition < 105) {
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePosition}px`;
 
@@ -39,12 +47,48 @@ class GameJumpMario {
             mario.style.width = '85px'
             mario.style.marginLeft = '47px'
 
+            buttonReset.removeAttribute('disabled');
+
             clearInterval(loop);
-         }
+         };
       }, 10)
+   }
 
-
+   creatScore(ponto) {
+      const p = document.createElement('p');
+      console.log(ponto)
+      p.innerHTML = 'Score: ' + ponto;
+      p.classList.add('score');
    }
 }
 
-const startGame = new GameJumpMario();
+function timeStartGame(mnsg, time) {
+   return new Promise((resolve, ) => {
+      setTimeout(() =>{ 
+         resolve(mnsg);
+      }, time)
+   })
+}
+
+function startGame() {
+   timeStartGame('0', 0)
+      .then(resposta => {
+         console.log(resposta);
+         return timeStartGame('1', 1000);
+      })
+      .then(resposta => {
+         console.log(resposta); 
+         return timeStartGame('2', 1000);
+      })
+      .then(resposta => {
+         console.log(resposta); 
+         return timeStartGame('GO!', 1000);
+      })
+      .then((resposta) => {
+         console.log(resposta);
+      })
+      .then(() => {
+         const startGame = new GameJumpMario();
+      })
+      .catch();
+}   
